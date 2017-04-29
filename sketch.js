f3b154c1a5a075350ae6652439e21d468c976cc3
KrinaM <krinamnn@gmail.com>
2017-04-30 01:38:13 +0200

    fixed reading problem

--- sketch.js
+++ sketch.js
@@ -17,13 +17,13 @@ var detector = function(row) {
   this.It = row.getNum("It");
   this.Bt = row.getNum("Bt");
 */
-  this.X = map(this.longitude,50.6, 51.5, 0, widthMap);
-  this.Y = map(this.lattitude,2.5, 5.6, heightCanvas, 0);
+  this.X = map(this.longitude,50.8, 51, 0, widthMap);
+  this.Y = map(this.lattitude,4, 4.6, heightCanvas, 0);
 
 }
 
 function preload() {
-  table = loadTable("DataMerged_short.csv","csv","header");
+  table = loadTable("obs10det.csv","csv","header");
   rows = table.getRows();
 }
 
@@ -31,21 +31,18 @@ function setup() {
   createCanvas(widthCanvas, heightCanvas);
   noLoop();
   detectorsImage = createGraphics(widthCanvas, heightCanvas);
-  for (var r = 0; r < rows.length; r+181) {
+  for (var r = 0; r < rows.length; r++) {
     var thisDetector = new detector(rows[r]);
     detectorsImage.noStroke();
     detectorsImage.fill(160,160,160,160);
-    detectorsImage.ellipse(thisDetector.X, thisDetector.Y, 3, 3);
+    detectorsImage.ellipse(thisDetector.X, thisDetector.Y, 10, 10);
     detectors.push(thisDetector);
   }
-  
-  console.log(rows[1]);
-  
-  
+//  console.log(rows[1]);
 }
 
 function draw() {
-    image(detectorsImage, 0, 0);
+  image(detectorsImage, 0, 0);
 }
 
 /*
