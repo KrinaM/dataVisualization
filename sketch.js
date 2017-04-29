93596874e848eaef80fb37b0dc58a7f9e1d5a9ac
KrinaM <krinamnn@gmail.com>
2017-04-29 23:18:14 +0200

    fixed detectors problem

--- sketch.js
+++ sketch.js
@@ -11,13 +11,15 @@ var widthMap = widthCanvas * .6;
 var widthBar = widthCanvas * .8;
 
 var detector = function(row) {
-  this.longitude = row.getNum("X");
-  this.lattitude = row.getNum("Y");
-  this.Vht = row.getNum("Vht");
+  this.longitude = row.getNum("Y");
+  this.lattitude = row.getNum("X");
+/*  this.Vht = row.getNum("Vht");
   this.It = row.getNum("It");
   this.Bt = row.getNum("Bt");
+*/
   this.X = map(this.longitude,-180,180,0,widthMap);
   this.Y = map(this.lattitude,-90,90, heightCanvas,0);
+
 }
 
 function preload() {
@@ -29,15 +31,15 @@ function setup() {
   createCanvas(widthCanvas, heightCanvas);
   noLoop();
   detectorsImage = createGraphics(widthCanvas, heightCanvas);
-  for (var r = 0; r < rows.length; r++) {
+  for (var r = 0; r < rows.length; r+181) {
     var thisDetector = new detector(rows[r]);
     detectorsImage.noStroke();
     detectorsImage.fill(160,160,160,160);
-    detectorsImage.ellipse(thisDetector.X,thisDetector.Y, 3, 3);
+    detectorsImage.ellipse(thisDetector.X, thisDetector.Y, 3, 3);
     detectors.push(thisDetector);
   }
   
-  console.log(rows[1])
+  console.log(rows[1]);
   
   
 }
