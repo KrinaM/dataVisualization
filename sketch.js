ceff8937eb50d83cca16eba94c1c0cfb45abfdae
KrinaM <krinamnn@gmail.com>
2017-04-30 22:11:40 +0200

    add main table

--- sketch.js
+++ sketch.js
@@ -9,15 +9,19 @@ var detectorsImage;
 
 var heightCanvas = 1200;
 var widthCanvas =  1200;
-var widthMap = widthCanvas * .5;
-var heightMap = heightCanvas * .5;
+var widthMap = widthCanvas * .45;
+var heightMap = heightCanvas * .45;
 // var widthBar = widthCanvas * .8;
+var heightTable = widthCanvas * .45;
+var widthTable = heightCanvas * .45;
+var margin = widthCanvas * .05;
+var bin = widthTable / 34.0;
+
 
 function preload() {
   table = loadTable("det120uniqM.csv","csv","header"); // det199uniq.csv
   rows = table.getRows();
   detectorsImage = createGraphics(widthCanvas, heightCanvas);
-  
   table2 = loadTable("obs10det.csv","csv","header")
   rows2 = table2.getRows();
 }
@@ -28,39 +32,35 @@ function setup() {
   
 //  all detectors inner ring
  for (var r = 0; r < rows.length; r++) {
- 
     var thisDetector = new detector(rows[r]);
-    
      /*   data.push(thisFlight.distance);  
     loc.push(0);
     loc.push(thisDetector.X);
     loc.push(thisDetector.Y);  */
- 
     detectorsImage.noStroke();
     detectorsImage.fill(160,160,160,160);
     detectorsImage.ellipse(thisDetector.X, thisDetector.Y, 10, 10);
- 
     detectors.push(thisDetector);
-    }
+  }
     
 // selected detectors
  for (var r = 0; r < rows2.length; r++) {
- 
     var selecDetector = new detector(rows2[r]);
-    
      /*   data.push(thisFlight.distance);  
     loc.push(0);
     loc.push(thisDetector.X);
     loc.push(thisDetector.Y);  */
- 
     detectorsImage.noStroke();
     detectorsImage.fill(0,160,160,160);
     detectorsImage.ellipse(selecDetector.X, selecDetector.Y, 10, 10);
- 
     detectors.push(selecDetector);
-    }
-    
-//  console.log(rows[1]);
+  }
+  translate(widthCanvas-widthTable-margin, 0)
+  rect(0, 0, widthTable, heightTable)
+  for (var i=1; i<34; i++ ) {
+    line(i*bin, 0, i*bin, heightTable)
+  }
+  
 }
 
 function draw() {
