a835487121fd89a1272801592bddfb9e3232b346
kafetzakid <danae.kafetzaki@gmail.com>
2017-04-30 20:45:40 +0200

    brussels ring

--- sketch.js
+++ sketch.js
@@ -2,13 +2,69 @@
 Authors: Krina Menounou, Danai Kafetzaki, Michael Christidis
 */
 
-var table, rows;
+var table, rows, rows2; // , minX, maxX, minY, maxY;
 var detectors = [];
+var detectorsImage;
 
-var heightCanvas = 400;
-var widthCanvas =  1600;
-var widthMap = widthCanvas * .6;
-var widthBar = widthCanvas * .8;
+var heightCanvas = 1200;
+var widthCanvas =  1200;
+var widthMap = widthCanvas * .5;
+var heightMap = heightCanvas * .5;
+// var widthBar = widthCanvas * .8;
+
+function preload() {
+  table = loadTable("det120uniqM.csv","csv","header"); // det199uniq.csv
+  rows = table.getRows();
+  detectorsImage = createGraphics(widthCanvas, heightCanvas);
+  
+  table2 = loadTable("obs10det.csv","csv","header")
+  rows2 = table2.getRows();
+}
+
+function setup() {
+  createCanvas(widthCanvas, heightCanvas);
+  noLoop();
+  
+//  all detectors inner ring
+ for (var r = 0; r < rows.length; r++) {
+ 
+    var thisDetector = new detector(rows[r]);
+    
+     /*   data.push(thisFlight.distance);  
+    loc.push(0);
+    loc.push(thisDetector.X);
+    loc.push(thisDetector.Y);  */
+ 
+    detectorsImage.noStroke();
+    detectorsImage.fill(160,160,160,160);
+    detectorsImage.ellipse(thisDetector.X, thisDetector.Y, 10, 10);
+ 
+    detectors.push(thisDetector);
+    }
+    
+// selected detectors
+ for (var r = 0; r < rows2.length; r++) {
+ 
+    var selecDetector = new detector(rows2[r]);
+    
+     /*   data.push(thisFlight.distance);  
+    loc.push(0);
+    loc.push(thisDetector.X);
+    loc.push(thisDetector.Y);  */
+ 
+    detectorsImage.noStroke();
+    detectorsImage.fill(0,160,160,160);
+    detectorsImage.ellipse(selecDetector.X, selecDetector.Y, 10, 10);
+ 
+    detectors.push(selecDetector);
+    }
+    
+//  console.log(rows[1]);
+}
+
+function draw() {
+    image(detectorsImage, 0, 0);
+}
 
 var detector = function(row) {
   this.longitude = row.getNum("Y");
@@ -17,36 +73,11 @@ var detector = function(row) {
   this.It = row.getNum("It");
   this.Bt = row.getNum("Bt");
 */
-  this.X = map(this.longitude,50.6, 51.5, 0, widthMap);
-  this.Y = map(this.lattitude,2.5, 5.6, heightCanvas, 0);
+  this.Y = map(this.longitude, 50.72422, 50.98837, widthMap, 0); // 50.86, 51
+  this.X = map(this.lattitude, 4.013617, 4.923672, 0, heightMap); // 4, 4.6
+};
 
-}
 
-function preload() {
-  table = loadTable("DataMerged_short.csv","csv","header");
-  rows = table.getRows();
-}
-
-function setup() {
-  createCanvas(widthCanvas, heightCanvas);
-  noLoop();
-  detectorsImage = createGraphics(widthCanvas, heightCanvas);
-  for (var r = 0; r < rows.length; r+181) {
-    var thisDetector = new detector(rows[r]);
-    detectorsImage.noStroke();
-    detectorsImage.fill(160,160,160,160);
-    detectorsImage.ellipse(thisDetector.X, thisDetector.Y, 3, 3);
-    detectors.push(thisDetector);
-  }
-  
-  console.log(rows[1]);
-  
-  
-}
-
-function draw() {
-    image(detectorsImage, 0, 0);
-}
 
 /*
 function mouseMoved() {
