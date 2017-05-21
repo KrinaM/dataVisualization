e12a79bfed8f3754be0a8ec00a045e564efbdf01
KrinaM <krinamnn@gmail.com>
2017-05-21 06:07:39 +0200

    averages

--- sketch.js
+++ sketch.js
@@ -56,6 +56,9 @@ var countDay = 0;
 var avgVht = [];
 var selectedDay = 1;
 
+var selectedDayPeak = 1; //selected day for average in ring
+var peakHour = 1; //selected peak hour (morning 6-9am or afternoon 3-6pm)
+var detOrderPeak = 1; // selected order of detector for average peak
 // object obs contains every row of the data set
 var obs = function(row) {
   this.longitude = row.getNum("Y");
@@ -70,6 +73,7 @@ var obs = function(row) {
   this.Month = row.getNum("MONTH");
   this.Time = row.getNum("TIME");
   this.Color = map(this.Vht, 0, 167, 255, 50);
+  this.ColorRing = map(this.Vht, 0, 167, 50, 100);
   this.Order = row.getNum("ORDER");
   this.Note = row.get("TEXT");
 }
@@ -99,6 +103,7 @@ function setup() {
   // ALL detectors image
   for (var r = 0; r < rowsALL.length; r++) {
     var thisDetectorALL = new obs(rowsALL[r]);
+    colorMode(RGB, 255);
     detectorsImageALL.noStroke();
     detectorsImageALL.fill(160, 160, 160, 160);
     detectorsImageALL.ellipse(thisDetectorALL.X, thisDetectorALL.Y, 10, 10);
@@ -112,6 +117,7 @@ function setup() {
   // Inner Ring detectors
   for (var r = 0; r < rowsIN.length; r++) {
     var thisDetectorIN = new obs(rowsIN[r]);
+    colorMode(RGB, 255);
     detectorsImage.noStroke();
     detectorsImage.fill(0, 0, 0, 160);
     detectorsImage.ellipse(thisDetectorIN.X, thisDetectorIN.Y, 10, 10);
@@ -122,7 +128,10 @@ function setup() {
   image(detectorsImage, 220, margin);
   //console.log(detectors.length)
 
-
+  var averPeak = computeAvgVhtPeak(1, 1, 2);
+  console.log(averPeak)
+    // Draw color reference bars
+  drawColorBars();
   // Draw Table
   translate(widthCanvas - widthTable, margin);
   // filter data according to Day and month
@@ -136,6 +145,7 @@ function setup() {
   for (var i = 0; i < 28; i++) {
     avgVht = computeAvgVht(i + 1, selectedDay);
     for (var k = 0; k < 180; k++) {
+      colorMode(RGB, 255);
       tableImage.noStroke();
       tableImage.fill(46, 139, 87, map(avgVht[k], 0, 167, 255, 50));
       tableImage.rect(widthBar * i, heightHour / 12 * k, widthBar, heightHour / 12)
@@ -168,27 +178,27 @@ function setup() {
 
   // Create buttons for choice of day in the table
   buttonMon = createButton('Monday');
-  buttonMon.position(20, 20);
+  buttonMon.position(widthCanvas * 0.4, 20);
   buttonMon.size(100, 20);
   buttonMon.mousePressed(chooseMon);
 
   buttonTue = createButton('Tuesday');
-  buttonTue.position(20, 40);
+  buttonTue.position(widthCanvas * 0.4, 40);
   buttonTue.size(100, 20);
   buttonTue.mousePressed(chooseTue);
 
   buttonWed = createButton('Wednesday');
-  buttonWed.position(20, 60);
+  buttonWed.position(widthCanvas * 0.4, 60);
   buttonWed.size(100, 20);
   buttonWed.mousePressed(chooseWed);
 
   buttonThu = createButton('Thursday');
-  buttonThu.position(20, 80);
+  buttonThu.position(widthCanvas * 0.4, 80);
   buttonThu.size(100, 20);
   buttonThu.mousePressed(chooseThu);
 
   buttonFri = createButton('Friday');
-  buttonFri.position(20, 100);
+  buttonFri.position(widthCanvas * 0.4, 100);
   buttonFri.size(100, 20);
   buttonFri.mousePressed(chooseFri);
 
@@ -197,9 +207,9 @@ function setup() {
 
 function draw() {
   background(0, 0);
-
   image(detectorsImageALL, 220, margin);
   image(detectorsImage, 220, margin);
+
   /*
     translate(widthCanvas - widthTable, margin);
     drawTableText();
@@ -211,6 +221,7 @@ function draw() {
 
     // Draw outer table rectangle
     push();
+    colorMode(RGB, 255);
     strokeWeight(4);
     stroke(255); //necessary to cover remaining blue stroke from interactivity
     fill(255);
@@ -219,23 +230,25 @@ function draw() {
 
     // Draw table bars
     push();
+    colorMode(RGB, 255);
     image(tableImage, widthCanvas - widthTable, margin);
     translate(widthCanvas - widthTable, margin);
     drawTableText();
-    translate(-widthCanvas+widthTable, -margin)
+    translate(-widthCanvas + widthTable, -margin)
     translate(.55 * widthCanvas + c * widthBar, margin);
     strokeWeight(4);
-    stroke(100, 255, 255);
+    stroke(255, 215, 0);
     noFill();
     rect(0, 0, widthBar, heightTable)
     pop();
 
     // Draw detectors
     push();
+    colorMode(RGB, 255);
     fill(255);
     image(detectorsImage, 220, margin);
     noStroke();
-    fill(255, 0, 0);
+    fill(255, 215, 0);
     ellipse(220 + detectors[3960 * c].X, margin + detectors[3960 * c].Y, 10, 10);
     pop();
 
@@ -246,6 +259,9 @@ function draw() {
     drawRing(c);
     pop();
   }
+  //    computeAvgVhtPeak(1, 1, 1);
+  //  console.log(computeAvgVhtPeak)
+
 }
 
 // sienna	#A0522D	(160,82,45,int)
@@ -263,11 +279,13 @@ function drawRing(c) {
   var cc = 0;
 
   // Draw ring
+
   translate(R2, R2);
   for (var i = 0; i < 44; i++) {
     for (var k = 0; k < 180; k++) {
       if ((k + 1) % 24 == 0) {
         strokeWeight(0.5);
+        colorMode(RGB, 255);
         stroke(210);
         noFill();
         ellipse(0, 0, (2 * (R2 + k * timeRay)) * cos(theta));
@@ -276,22 +294,34 @@ function drawRing(c) {
       strokeMapWeight = map(selDet[c * cc * 180 + k].Vht, 167, 0, 2, 15);
       strokeWeight(strokeMapWeight);
 
+      colorMode(HSB);
       if (cc > 21) {
-        stroke(148, 0, selDet[c * cc * 180 + k].Color) // dark violet
+        stroke(210, 100, selDet[c * cc * 180 + k].ColorRing) // blue = March
       } else {
-        stroke(255, 215, selDet[c * cc * 180 + k].Color) // gold
+        stroke(22, 100, selDet[c * cc * 180 + k].ColorRing) // orange = October
       }
 
       // Yellow line starts at 0 countDay (First date in the dataset)
       line((R2 + k * timeRay) * sin(theta * (i + 1)), (R2 + k * timeRay) * (-cos(theta * (i + 1))), (R2 + (k + 1) * timeRay) * sin(theta * (i + 1)), (R2 + (k + 1) * timeRay) * (-cos(theta * (i + 1))));
 
-      /* 
-            textFont();
-            textSize();
-            fill(50);
-            text(countDay.toString(), (R2 + (k+1) * timeRay) * sin(theta * (i + 1)), 
-            (R2 + (k+1) * timeRay) * (-cos(theta * (i + 1))) );
-      */
+    //       var p1 = computeAvgVhtPeak(c+1, i+1, 1);
+    //      var p2 = computeAvgVhtPeak(c+1, i+1, 1);
+    }
+    noStroke()
+    fill(0);
+    if (cc % 5 == 0 && cc < 22) {
+      if (cc < 5) {
+        text("M", (R2 + 10 + 181 * timeRay) * sin(theta * (i + 1)), (R2 + 10 + 181 * timeRay) * (-cos(theta * (i + 1))));
+      } else {
+        text("M", (R2 + i * 0.2 + 10 + 181 * timeRay) * sin(theta * (i + 1)), (R2 + i * 0.2 + 10 + 181 * timeRay) * (-cos(theta * (i + 1))));
+      }
+    }
+    if ((cc - 22) % 5 == 0 && cc >= 22) {
+      if (cc > 38) {
+        text("M", (R2 + 10 + 181 * timeRay) * sin(theta * (i + 1)), (R2 + 10 + 181 * timeRay) * (-cos(theta * (i + 1))));
+      } else {
+        text("M", (R2 + i * 0.2 + 10 + 181 * timeRay) * sin(theta * (i + 1)), (R2 + i * 0.2 + 10 + 181 * timeRay) * (-cos(theta * (i + 1))));
+      }
     }
     cc++;
   }
@@ -333,6 +363,37 @@ function computeAvgVht(detOrder, dayOfWeek) {
 }
 
 
+function computeAvgVhtPeak(detOrderPeak, selectedDayPeak, peakHour) {
+  // select detector and specific day
+  if (selectedDayPeak <= 22) {
+    var selDayPeak = detectors.filter(function(obj) {
+      return (obj.Month == 3 && obj.Day == selectedDayPeak && obj.Order == detOrderPeak);
+    });
+  } else {
+    var selDayPeak = detectors.filter(function(obj) {
+      return (obj.Month == 10 && obj.Day == selectedDayPeak && obj.Order == detOrderPeak);
+    });
+
+  }
+  var sumPeak = 0;
+  // compute average Vht
+  if (peakHour == 1) {
+    for (var i = 12; i < 48; i++) {
+      sumPeak += selDayPeak[i].Vht
+    }
+  } else {
+    for (var i = 108; i < 108 + 36; i++) {
+      sumPeak += selDayPeak[i].Vht
+    }
+
+  }
+  avgPeak = sumPeak / 36;
+  return avgPeak;
+}
+
+
+
+
 function chooseMon() {
   selectedDay = 1;
 }
@@ -368,16 +429,65 @@ function drawTableText() {
     noStroke();
     text(detectors[3960 * j].Note, j * widthBar, -margin * 0.5);
   }
+}
 
+function drawColorBars() {
+  translate(0, -100);
+  for (var i = 0; i < 200; i++) {
+    colorMode(HSB);
+    var colMapRing = map(i, 0, 200, 50, 100);
+    //noStroke()
+    strokeWeight(widthBar / 2);
+    stroke(210, 100, colMapRing);
+    point(margin, 160 + i * heightTable / 800);
+    strokeWeight(widthBar / 2);
+    stroke(22, 100, colMapRing);
+    point(margin + widthBar, 160 + i * heightTable / 800);
+    var colMapTable = map(i, 0, 200, 255, 50);
+    noStroke();
+    colorMode(RGB, 225, 255, 255, 255);
+    fill(46, 139, 87, colMapTable);
+    rect(margin + 2 * widthBar, 160 + i * heightTable / 800, widthBar / 2, heightTable / 800);
+  }
+  noStroke()
+  fill(0)
+  textSize(10);
+  text("Lower Speed", margin - 10, 150)
+  text("Higher Speed", margin - 10, 175 + heightTable / 4)
+  translate(0, 20)
+  strokeWeight(widthBar / 2);
+  colorMode(HSB)
+  stroke(210, 100, 175)
+  point(margin, 160 + heightTable / 4 + margin)
+  noStroke()
+  fill(0)
+  textSize(10);
+  text("March", 1.5 * margin, 165 + heightTable / 4 + margin)
+  stroke(22, 100, 175)
+  point(margin, 180 + heightTable / 4 + margin)
+  noStroke()
+  fill(0)
+  textSize(10);
+  text("October", 1.5 * margin, 20 + 165 + heightTable / 4 + margin)
+  noStroke();
+  colorMode(RGB, 225, 255, 255, 255);
+  fill(46, 139, 87, 102);
+  rect(margin - 5, 35 + 160 + heightTable / 4 + margin, widthBar / 2, widthBar / 2);
+  noStroke()
+  fill(0)
+  textSize(10);
+  text("Average Speed", 1.5 * margin, 35 + 170 + heightTable / 4 + margin)
+
+  translate(0, 80);
 }
 
 function mouseClicked() {
   if (mouseX > .55 * widthCanvas && mouseX < .55 * widthCanvas + numDetIN * widthBar) {
     trig = 1;
     clear();
+    redraw();
+    drawColorBars();
   } else {
     trig = 0;
   }
-
-  redraw();
 }
